4bde9993ba7d43c5c28b1ebb614d447666fefc6e
Xaknazarov Sharasul <741922@bk.ru>
2024-02-27 10:38:27 +0500

    add folder sem2_task1

---
+++ sem2_task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int FirstDigit = num / 100;
+int SecondDigit = num % 10;
+
+if (num >= 100 && num <= 999 || num >= -999 && num <= -100) // знак || означает логическое значение - или
+{
+    int result = FirstDigit*10 + SecondDigit;
+    Console.WriteLine($"a = {num} => {result}");
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
