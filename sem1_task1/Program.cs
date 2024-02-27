a44751c5939078d4c8227a9c1ddcf2ea22145fc6
Xaknazarov Sharasul <741922@bk.ru>
2024-02-27 10:37:35 +0500

    add folder sem1_task1

---
+++ sem1_task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая на вход принимает два целых числа и проверяет,
+// является ли первое число квадратом второго. 
+// a = 25, b = 5 => да 
+// a = 2, b = 10 => нет 
+// a = 9, b = -3  => да 
+// a = -3, b = 9   => нет
+
+
+
+Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = num1 == num2 * num2;
+
+string resultStr = result ? "Да" : "Нет";
+
+Console.WriteLine($"a={num1}, b={num2} => {resultStr}");
+
+
+// Console.WriteLine("Введите первое целое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите первое целое число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num2 * num2 == num1)
+// {
+//     Console.WriteLine($"a={num1}, b={num2} => да");
+// }
+// else
+// {
+//     Console.WriteLine($"a={num1}, b={num2} => нет");
+// }
