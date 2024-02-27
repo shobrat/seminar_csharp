214c73786eacf8f985e69bc0ad6c6985f72c2526
Xaknazarov Sharasul <741922@bk.ru>
2024-02-27 10:37:49 +0500

    add folder sem1_task2

---
+++ sem1_task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    num = num * -1;
+}
+
+int size = num;
+//int otr = - num;
+int count = -num;
+
+while (count < size)
+{
+
+    Console.Write($"{count} ");
+    count++;
+}
+
