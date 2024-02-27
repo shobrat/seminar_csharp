1e9e852e256a4c17d7d667f32f890ba6a26269e7
Xaknazarov Sharasul <741922@bk.ru>
2024-02-27 10:38:00 +0500

    add folder sem1_task3

---
+++ sem1_task3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int sum = num / 100 + num % 10;
+    Console.WriteLine($"{sum}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
