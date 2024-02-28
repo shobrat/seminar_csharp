0d557ae0e09052367133da76a4cf22485846aac6
Xaknazarov Sharasul <741922@bk.ru>
2024-02-28 14:43:38 +0500

    add folder sem3_task3

---
+++ sem3_task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найдите произведения пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д. Результат запишите в новый массив. 
+
+// Пример [1 3 2 4 2 3] => [3 6 8] [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
+
+
+int[] array2 = { 1, 3, 2, 4, 2, 3 };
+int[] array = { 2, 3, 1, 7, 5, 6, 3 };
+int[] newArray = new int[array.Length / 2]; //Свойство элемента array.Length
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    newArray[i] = array[i] * array[array.Length - 1 - i];
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < newArray.Length; i++)
+{
+ 
+    Console.Write($"{newArray[i]} ");
+}
---
+++ sem3_task4/Program.cs
@@ -0,0 +1,48 @@
+﻿// Дано натуральное трёхзначное число. Создайте массив, 
+// состоящий из цифр этого числа. Младший разряд числа 
+// должен располагаться на 0м индексе массива, 
+// старший – на 2-м. 
+
+// Пример 456  =>  [6 5 4] 781  =>  [1 8 7]
+
+// Console.WriteLine("Введите трехзначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count = 0;
+// int size = 3;
+// int num1 = num / 100;
+// int num2 = num % 10;
+// int num3 = num / 10 % 10;
+
+// while (count < size)
+// {
+//     //Console.Write($"{num} => {num2} {num3} {num1}");
+//     count = count + 1;
+// }
+// Console.Write($"{num} => [{num2} {num3} {num1}]");
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[3];
+int i = 0;
+
+if (num >= 100 && num <= 999)
+{
+
+    while (num > 0)
+    {
+        Array[i] = num % 10;
+        i++;
+        num /= 10; //num = num / 10; 
+    }
+    for (int j = 0; j < Array.Length; j++)
+    {
+        Console.Write($"{Array[j]} ");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+
+
+
