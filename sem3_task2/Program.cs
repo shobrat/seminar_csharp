31de4955cb81c024850a11aa2e21148d56b9c54e
Xaknazarov Sharasul <741922@bk.ru>
2024-02-28 12:16:18 +0500

    add folder sem3_task2

---
+++ sem3_task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из промежутка [-10, 10]. 
+// Замените отрицательные элементы на положительные, 
+// а положительные на отрицательные. 
+// Пример [1 -5 6] => [-1 5 -6]
+
+int[] array = new int[10] {1, -5, 6, 3, -4, 1, 9, -6, 2, -7};
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1; //array[i] = array[i] * -1;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
