dd1aa7add136e125468346681293671f5bd57f82
Sharasul <sh.haqnazarov@bipartners.uz>
2024-03-02 21:46:27 +0500

    add folder sem3_task1

--- sem3_task1/Program.cs
+++ sem3_task1/Program.cs
@@ -1,27 +1,25 @@
-﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
-// Если второе число некратно первому, то программа выводит остаток от деления.
-// Примеры
-// 14, 5 => нет, 4
-// 16, 8 => да
-// 4, 3 => нет, 1
+﻿// Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве. 
+// Программа должна выдать ответ: Да/Нет. 
 
+// Примеры [1 3 4 19 3], 8 => Нет [-4 3 4 1], 3 => Да
+// int[] array1= new int[8]; //В таком случае в массиве будут только нули
+// int[] array2 = new int[8] { 4, 6, 7, 9, 3, 2, 4, 5 };
+// int[] array3 = new int[] { 4, 6, 7, 8, 9, 0, 3, 2 };
 
-Console.WriteLine("Введите первое целое число");
-int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте целое число для поиска в массиве");
+int targetNum = Convert.ToInt32(Console.ReadLine());
+int[] array = { 5, 6, 7, 8, 2, 3, 4, 5, 6 };
+bool res = false;
 
-Console.WriteLine("Введите первое целое число");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-int remainder = num1 % num2;
-
-if (remainder == 0)
+for (int i = 0; i < array.Length; i++)
 {
-    //Первое число является кратным второму
-    Console.WriteLine($"{num1}, {num2} => Да");
-
+    if (array[i] == targetNum)
+    {
+        res = true;
+        break;
+    }
 }
 
-else 
-{
-    Console.WriteLine($"{num1}, {num2} => {remainder}");
-}
+string result = res ? "Да" : "Нет"; //тернарный оператор
+Console.WriteLine(result); 
